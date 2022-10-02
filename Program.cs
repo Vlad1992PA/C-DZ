a9c52cd7a1c47909361fbc99db451c4fa8659698
Vlad <60320358@mail.ru>
2022-10-02 13:18:09 +0300

    DZ4

--- Program.cs
+++ Program.cs
@@ -1,21 +1,48 @@
-﻿/* задача №1
-*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3*/
-
-Console.Write("ведите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("ведите second число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2)
+﻿internal class Program
 {
-    Console.WriteLine("max = " + num1);
-}
-else
-{
-    Console.WriteLine("max = " + num2);
-}
+    private static void Main(string[] args)
+    {
+        /* задача C# №2
+        Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+        2, 3, 7 -> 7
+        44 5 78 -> 78
+        22 3 9 -> 22*/
+   
+        Console.Write("ведите first число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
 
+        Console.Write("ведите second число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("ведите the third число: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int max = 0;
+      
+        if (num1 > num2)
+        {
+           if (num1 > num3) 
+           {
+            max = num1;
+           }
+
+           else
+           {
+            max = num3;
+           }
+        }
+
+        else if (num2 > num3)
+        {
+            max = num2; 
+        }
+
+        else
+        {
+            max = num3;
+        }
+            
+        Console.WriteLine("max = " + max);        
+    }
+}
