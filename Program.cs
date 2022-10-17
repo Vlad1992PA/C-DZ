5eb8065f5b43818b64570e1267ec697d928990f7
Vlad1992PA <106955857+Vlad1992PA@users.noreply.github.com>
2022-10-17 16:38:36 +0300

    Update Program.cs
    Не могу понять почему при выводе второго интервала выдает ошибку!

--- Program.cs
+++ Program.cs
@@ -1,35 +1,40 @@
-/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-452 -> 11
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
-82 -> 10
-
-9012 -> 12*/
-
+6, 1, 33 -> [6, 1, 33]
+*/
 using System;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        
-        
-        int Met(string nums)
-        {
-            int sum = 0;
-            for  (int i = 0; i< nums.Length; i++)
-            {
-                int x = Convert.ToInt32(nums[i].ToString());
-                sum = sum + x;
-            }
-            return sum;
+        int i;
+        int[] a = new int[8];
+        Console.WriteLine("Введите массив чисел: ");
+        for (i = 0; i < 8; i++)
+       {
+            a[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введитеследующее число массивасел: ");
         }
-            
-            Console.WriteLine("Введите число");
-            string num = Console.ReadLine();
-            int summa = Met(num);
+        void Met()
+        {
+            int[] b = a;
+            int start = 0, end = 5;
+            var segment = new ArraySegment<int>(b, start, end);
+            Console.WriteLine(String.Join(", ", segment));
+        }
+        void Met2()
+        {
+            int[] c = a;
+            int start = 5, end = 6;
+            var segment = new ArraySegment<int>(c, start, end);
+            Console.WriteLine(String.Join(", ", segment));
+        }
+        Met();
+        Met2();
         
-            Console.WriteLine("Hello, world! ^ " + summa);
         
     }
 }
