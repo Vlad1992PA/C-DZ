0c3cabba73264917c710b9bcff1e57871902199a
Vlad1992PA <106955857+Vlad1992PA@users.noreply.github.com>
2022-10-20 16:47:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,40 +1,97 @@
-/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
 
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
 
-6, 1, 33 -> [6, 1, 33]
-*/
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+
+//Задача 34: 
+
+/*
 using System;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        int i;
-        int[] a = new int[8];
-        Console.WriteLine("Введите массив чисел: ");
-        for (i = 0; i < 8; i++)
-       {
-            a[i] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введитеследующее число массивасел: ");
-        }
-        void Met()
-        {
-            int[] b = a;
-            int start = 0, end = 5;
-            var segment = new ArraySegment<int>(b, start, end);
-            Console.WriteLine(String.Join(", ", segment));
-        }
-        void Met2()
-        {
-            int[] c = a;
-            int start = 5, end = 6;
-            var segment = new ArraySegment<int>(c, start, end);
-            Console.WriteLine(String.Join(", ", segment));
-        }
-        Met();
-        Met2();
         
+        int count = 0;
         
+        int Met(int[] array)
+        {
+            for  (int i = 0; i< array.GetLength(0); i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+        return count;
+        }
+        
+        int[] Met2()
+        {
+            int[] array = new int [4];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(100,999);
+            }
+            return  array;
+
+        }
+        
+            
+            Met(Met2());
+            Console.WriteLine("количество чётных чисел в массиве" + count);
     }
-}
+    */
+    
+//Задача 36: 
+    /*
+    using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        int sum = 0;
+        
+        int Met(int[] array)
+        {
+            for  (int i = 0; i< array.GetLength(0); i++)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    sum= sum + array[i];
+                }
+
+            }
+        return sum;
+        
+        }
+        
+        int[] Met2()
+        {
+            int[] array = new int [4];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(100,999);
+            }
+            return  array;
+
+        }
+        
+            
+            Met(Met2());
+            Console.WriteLine("sum=" + sum);
+    }
+    */
+
